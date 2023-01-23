9000a97144c7077a5dd826e11572569c7f0d06be
duyaex <oredd724@gmail.com>
2023-01-23 11:51:00 +0530

    Image

--- Components/UI_Interface/Files/Loader.jsx
+++ Components/UI_Interface/Files/Loader.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import circles from '../../Images/circles.gif'
+import Image from 'next/image'
 export default function Loader() {
   return (
     <div className='flex justify-center items-center w-full py-2'>
