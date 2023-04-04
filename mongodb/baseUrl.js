5d0dbd334e196db2c0b9feb0fe2da21bf135f191
duyaex <oredd724@gmail.com>
2023-04-04 09:43:22 +0530

    some Changes

--- mongodb/baseUrl.js
+++ mongodb/baseUrl.js
@@ -1,3 +1,4 @@
-const baseUrl = "https://unrevealpics.vercel.app"
-
+const baseUrl = process.env.NODE_ENV === "production" ? "https://unrevealpics.vercel.app" : "http://localhost:3000"
+    // const baseUrl = process.env.NODE_ENV === 'production' ? "" : "http://localhost:3000"
+console.log("The environment is ", process.env.NODE_ENV)
 export default baseUrl;
