6abc4db701aaa1099c1442fd2aac7a23fe7237ae
duyaex <oredd724@gmail.com>
2023-04-07 11:29:38 +0530

    file postions changes

--- pages/api/addToSrchReqPh/addToSrch.js
+++ pages/api/addToSrchReqPh/addToSrch.js
@@ -14,7 +14,7 @@ export default function AddToSearchRequirePhotos(req, res) {
 const addToSrch = async(req, res) => {
     const { term } = req.body
     const srchArr = []
-    const inData = fs.readFileSync('search_require_photos.json')
+    const inData = fs.readFileSync('/public/search_require_photos.json')
         // console.log("the initial data ",JSON.parse(inData))
     const objData = JSON.parse(inData)
     const addSrch = [`${term}`]
@@ -24,7 +24,7 @@ const addToSrch = async(req, res) => {
     // console.log("the srchArr",srchArr)
     const addingData = srchArr.concat(addSrch)
         // console.log("the finale data is ",addingData)
-    fs.writeFileSync('search_require_photos.json', JSON.stringify(addingData), () => {})
+    fs.writeFileSync('/public/search_require_photos.json', JSON.stringify(addingData), () => {})
     await res.status(201).json({
         success: true,
         message: "Search has been added"
@@ -36,7 +36,7 @@ const clearAllSrches = async(req, res) => {
     // console.log("the srchArr",srchArr)
     const addingData = []
         // console.log("the finale data is ",addingData)
-    fs.writeFileSync('recent_searches.json', JSON.stringify(addingData), () => {})
+    fs.writeFileSync('/public/recent_searches.json', JSON.stringify(addingData), () => {})
     await res.status(200).json({
         success: true,
         message: "Search has been cleared"
@@ -45,7 +45,7 @@ const clearAllSrches = async(req, res) => {
 
 const getAllReqPhSrches = async(req, res) => {
     const searches = []
-    const data = fs.readFileSync('search_require_photos.json')
+    const data = fs.readFileSync('/public/search_require_photos.json')
     const objData = await JSON.parse(data)
     for (let key in objData) {
         searches.push(objData[key])
