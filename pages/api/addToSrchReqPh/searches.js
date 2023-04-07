6b17d7d2e2d38ec10e554674ad82f6b0809c5442
duyaex <oredd724@gmail.com>
2023-04-07 11:03:50 +0530

    searches changes

--- pages/api/addToSrchReqPh/searches.js
+++ pages/api/addToSrchReqPh/searches.js
@@ -10,11 +10,11 @@ export default function AllSrchesGetAndPost(req, res) {
 }
 const getAllSearches = async(req, res) => {
     const searches = []
-    const data = await fs.readFileSync('recent_searches.json')
-    const objData = await JSON.parse(data)
-    for (let key in objData) {
-        searches.push(objData[key])
-    }
+        // const data = await fs.readFileSync('recent_searches.json')
+        // const objData = await JSON.parse(data)
+        // for (let key in objData) {
+        //     searches.push(objData[key])
+        // }
     await res.status(200).json({
         success: true,
         searches: searches
@@ -24,17 +24,17 @@ const getAllSearches = async(req, res) => {
 const addToSrches = async(req, res) => {
     const { term } = req.body
     const srchArr = []
-    const inData = await fs.readFileSync('recent_searches.json')
-        // console.log("the initial data ",JSON.parse(inData))
-    const objData = await JSON.parse(inData)
-    const addSrch = [`${term}`]
-    for (let key in objData) {
-        srchArr.push(objData[key])
-    }
-    // console.log("the srchArr",srchArr)
-    const addingData = srchArr.concat(addSrch)
-        // console.log("the finale data is ",addingData)
-    await fs.writeFileSync('recent_searches.json', JSON.stringify(addingData), () => {})
+        // const inData = await fs.readFileSync('recent_searches.json')
+        //     // console.log("the initial data ",JSON.parse(inData))
+        // const objData = await JSON.parse(inData)
+        // const addSrch = [`${term}`]
+        // for (let key in objData) {
+        //     srchArr.push(objData[key])
+        // }
+        // // console.log("the srchArr",srchArr)
+        // const addingData = srchArr.concat(addSrch)
+        //     // console.log("the finale data is ",addingData)
+        // await fs.writeFileSync('recent_searches.json', JSON.stringify(addingData), () => {})
     await res.status(200).json({
         success: true,
         message: "Search has been added"
